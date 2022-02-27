85021255318dda0dc5e838845f2067faadc193fd
publicdevop2019 <publicdevop2019hw@gmail.com>
2022-02-26 23:49:34 -0500

    add new csrf ep

---
+++ mt-access/src/main/java/com/mt/access/resource/CSRFResource.java
@@ -0,0 +1,18 @@
+package com.mt.access.resource;
+
+import com.mt.access.application.cors_profile.representation.CORSProfileRepresentation;
+import com.mt.common.domain.model.restful.SumPagedRep;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(produces = "application/json", path = "csrf")
+public class CSRFResource {
+    //required to get csrf cookie
+    @GetMapping
+    public ResponseEntity<SumPagedRep<CORSProfileRepresentation>> csrf() {
+        return ResponseEntity.ok().build();
+    }
+}
