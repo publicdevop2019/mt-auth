1944becd23f0515c207dfe4e65e25484c5870e46
publicdevop2019 <publicdevop2019hw@gmail.com>
2022-02-27 22:45:43 -0500

    use get for csrf

--- mt-access/src/main/java/com/mt/access/resource/CSRFResource.java
+++ mt-access/src/main/java/com/mt/access/resource/CSRFResource.java
@@ -4,7 +4,6 @@ import com.mt.access.application.cors_profile.representation.CORSProfileRepresen
 import com.mt.common.domain.model.restful.SumPagedRep;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(produces = "application/json", path = "csrf")
 public class CSRFResource {
     //required to get csrf cookie
-    @PostMapping
+    @GetMapping
     public ResponseEntity<SumPagedRep<CORSProfileRepresentation>> csrf() {
         return ResponseEntity.ok().build();
     }
