2dbeb44c29bc674707dfabd5673a9020e4fa7073
publicdevop2019 <publicdevop2019hw@gmail.com>
2022-05-02 00:44:38 -0400

    add mfa part2

--- mt-access/src/main/java/com/mt/access/domain/model/user/event/UserMfaNotificationEvent.java
+++ mt-access/src/main/java/com/mt/access/domain/model/user/event/UserMfaNotificationEvent.java
@@ -4,7 +4,13 @@ import com.mt.access.domain.model.user.User;
 import com.mt.common.domain.model.domain_event.DomainEvent;
 
 public class UserMfaNotificationEvent extends DomainEvent {
-    public UserMfaNotificationEvent(User user) {
 
+    public static final String USER_MFA_NOTIFICATION = "user_mfa_notification";
+    public static final String name = "USER_MFA_NOTIFICATION";
+
+    public UserMfaNotificationEvent(User user) {
+        super(user.getUserId());
+        setTopic(USER_MFA_NOTIFICATION);
+        setName(name);
     }
 }
