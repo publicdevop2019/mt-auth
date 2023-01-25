620667bf716815ef1f6688882fce7987c56b18e6
manytree <publicdevop2019hw@gmail.com>
2023-01-25 12:27:58 +0800

    improve notification time display

--- mt-ui/src/app/components/card-notification/card-notification.component.ts
+++ mt-ui/src/app/components/card-notification/card-notification.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { IBellNotification, MessageService } from 'src/app/services/message.service';
-import { DateTime } from "luxon";
+import { DateTime, ToRelativeUnit } from "luxon";
 import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-card-notification',
@@ -12,7 +12,7 @@ export class CardNotificationComponent implements OnInit {
   @Input() length: number;
   @Input() index: number;
   parsedDate: string;
-  constructor(public translate: TranslateService,private msgSvg:MessageService) {
+  constructor(public translate: TranslateService, private msgSvg: MessageService) {
   }
   ngOnInit(): void {
     let resolved: string;
@@ -21,14 +21,26 @@ export class CardNotificationComponent implements OnInit {
     } else {
       resolved = 'en-Us'
     }
-    let resolvedUnit: 'seconds' | 'minutes' = 'seconds';
+    let resolvedUnit: ToRelativeUnit = 'seconds';
     if (DateTime.fromMillis(this.value.date).diffNow('seconds').seconds < -60) {
       resolvedUnit = 'minutes'
+    }
+    if (DateTime.fromMillis(this.value.date).diffNow('minutes').minutes < -60) {
+      resolvedUnit = 'hours'
+    }
+    if (DateTime.fromMillis(this.value.date).diffNow('hours').hours < -24) {
+      resolvedUnit = 'days'
+    }
+    if (DateTime.fromMillis(this.value.date).diffNow('days').days < -30) {
+      resolvedUnit = 'months'
+    }
+    if (DateTime.fromMillis(this.value.date).diffNow('months').months < -12) {
+      resolvedUnit = 'years'
     }
     const parsed = DateTime.fromMillis(this.value.date).setLocale(resolved).toRelativeCalendar({ unit: resolvedUnit });
     this.parsedDate = parsed;
   }
-  dismissMsg(event:Event){
+  dismissMsg(event: Event) {
     event.stopPropagation();
     this.msgSvg.dismiss(this.value)
   }
