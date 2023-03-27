455d6a87dfb3837af94df3bfb29b02f35cc232be
manytree <publicdevop2019hw@gmail.com>
2023-03-28 00:27:26 +0800

    update build script

--- script/build/mt-proxy-build.sh
+++ script/build/mt-proxy-build.sh
@@ -2,5 +2,4 @@ cd ../../
 git checkout main
 git fetch
 git pull
-docker build -f mt-proxy/Dockerfile . -t publicdevop2019/mt-proxy:latest --no-cache
-docker push publicdevop2019/mt-proxy:latest
+docker build -f mt-proxy/Dockerfile . -t mt-access/mt-proxy:latest --no-cache
