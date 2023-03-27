39f376cf2129ae84ab7f771e377a5863742c311d
manytree <publicdevop2019hw@gmail.com>
2023-03-28 00:27:00 +0800

    update build script

--- script/build/mt-access-build.sh
+++ script/build/mt-access-build.sh
@@ -2,5 +2,4 @@ cd ../../
 git checkout main
 git fetch
 git pull
-docker build -f mt-access/Dockerfile . -t publicdevop2019/mt-access:latest --no-cache
-docker push publicdevop2019/mt-access:latest
+docker build -f mt-access/Dockerfile . -t mt-auth/mt-access:latest --no-cache
