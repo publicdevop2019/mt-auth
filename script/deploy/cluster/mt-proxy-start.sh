22594ffa3e292bb49c45bd81e53440b10bd3df2f
manytree <publicdevop2019hw@gmail.com>
2023-03-29 22:01:03 +0800

    update deploy script

--- script/deploy/cluster/mt-proxy-start.sh
+++ script/deploy/cluster/mt-proxy-start.sh
@@ -12,6 +12,6 @@ LOG_CONFIG=-Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLog
 REGISTRY_CONFIG="--eureka.client.serviceUrl.defaultZone=$REGISTRY_LIST --eureka.instance.ip-address=$INSTANCE_IP"
 INFRA_CONFIG="--mt.url.support.dis_lock=redis://$INFRA_IP:6378 --mt.url.support.mq=$INFRA_IP --spring.redis.host=$INFRA_IP --spring.redis.port=6379"
 DOCKER_CONFIG="-td --rm -v $PWD/./logs/proxy:/logs -p 8111:8111 --log-opt max-size=10m --log-opt max-file=5"
-VM_CONFIG="-Xmx500m -Xms500m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/logs/$(date +"%Y_%m_%d_%I_%M_%p%z")_heapdump.hprof -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
+VM_CONFIG="-Xmx500m -Xms500m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/logs/$(date +"%Y_%m_%d_%I_%M_%p%z")_heapdump.hprof  -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
 IMAGE_CONFIG="--name proxy mt-auth/mt-proxy:latest $VM_CONFIG -jar Proxy.jar"
 docker run $DOCKER_CONFIG $IMAGE_CONFIG $LOG_CONFIG $REGISTRY_CONFIG $INFRA_CONFIG
