255c3ef0f384598fc4f6923e18f96f146bbab34c
manytree <publicdevop2019hw@gmail.com>
2023-03-31 00:31:31 +0800

    update new domain

--- mt-ui/src/environments/environment.prod.ts
+++ mt-ui/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   mode: 'online' as 'online' | 'offline',
   validation: 'on' as 'on' | 'off',
-  serverUri: 'https://api.duoshu.org',
+  serverUri: 'https://api.duoshu.xyz',
   loginClientId: '0C8AZZ16LZB4',
   registerClientId: '0C8B00098WLD',
   clientSecret: '',
