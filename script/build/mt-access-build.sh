e10909f617464083c10107369aba2c3020c3e94a
manytree <publicdevop2019hw@gmail.com>
2023-04-05 13:02:17 +0800

    update build script

--- script/build/mt-access-build.sh
+++ script/build/mt-access-build.sh
@@ -4,6 +4,7 @@ echo '--------sync git--------'
 #git fetch
 #git pull
 echo '--------prepare maven--------'
+mkdir $HOME/.m2
 cp ~/mt-auth/script/build/settings.xml $HOME/.m2
 echo '--------create mt-common jar--------'
 docker run -it --rm -v ~/mt-auth:/usr/src/temp -v "$HOME/.m2":/root/.m2 -w /usr/src/temp/mt-common maven:3.6.0-jdk-11 mvn dependency:go-offline -B clean install
