9252c89260a99c7a56bc9e710aa001860d372fbb
manytree <publicdevop2019hw@gmail.com>
2023-04-06 00:52:25 +0800

    update build script

--- script/build/mt-access-build.sh
+++ script/build/mt-access-build.sh
@@ -4,11 +4,12 @@ echo '--------sync git--------'
 #git fetch
 #git pull
 echo '--------prepare maven--------'
-mkdir $HOME/.m2
+mkdir -p $HOME/.m2
 cp ~/mt-auth/script/build/settings.xml $HOME/.m2
 echo '--------create mt-common jar--------'
 docker run -it --rm -v ~/mt-auth:/usr/src/temp -v "$HOME/.m2":/root/.m2 -w /usr/src/temp/mt-common maven:3.6.0-jdk-11 mvn dependency:go-offline -B clean install
 echo '--------create mt-access jar--------'
 docker run -it --rm -v ~/mt-auth:/usr/src/temp -v "$HOME/.m2":/root/.m2 -w /usr/src/temp/mt-access maven:3.6.0-jdk-11 mvn dependency:go-offline -B clean package
 echo '--------create mt-access docker image--------'
-docker build -f ~/mt-auth/mt-access/Dockerfile . -t mt-auth/mt-access:latest --no-cache
+cd ~/mt-auth # required for docker build to work
+docker build -f ./mt-access/Dockerfile . -t mt-auth/mt-access:latest --no-cache
--- script/build/mt-proxy-build.sh
+++ script/build/mt-proxy-build.sh
@@ -4,9 +4,10 @@ echo '--------sync git--------'
 #git fetch
 #git pull
 echo '--------prepare maven--------'
-mkdir $HOME/.m2
+mkdir -p $HOME/.m2
 cp ~/mt-auth/script/build/settings.xml $HOME/.m2
 echo '--------create mt-proxy jar--------'
 docker run -it --rm -v ~/mt-auth:/usr/src/temp -v "$HOME/.m2":/root/.m2 -w /usr/src/temp/mt-proxy maven:3.6.3-jdk-11 mvn dependency:go-offline -B clean package
 echo '--------create mt-proxy docker image--------'
+cd ~/mt-auth # required for docker build to work
 docker build -f ~/mt-auth/mt-proxy/Dockerfile . -t mt-auth/mt-proxy:latest --no-cache
