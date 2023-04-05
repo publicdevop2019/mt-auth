900df4602945c8f346b7d1bd35eb96aa40819175
manytree <publicdevop2019hw@gmail.com>
2023-04-05 12:21:16 +0800

    update build script

--- script/build/mt-access-build.sh
+++ script/build/mt-access-build.sh
@@ -1,14 +1,13 @@
 cd ../../
 echo 'sync git'
-git checkout main
-git fetch
-git pull
+#git checkout main
+#git fetch
+#git pull
 echo 'prepare maven'
-mkdir .m2
-cp ./script/build/settings.xml .m2
+cp ./script/build/settings.xml $HOME/.m2
 echo 'create mt-common jar'
-docker run -it --rm --name builder -v .m2:/root/.m2 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven/mt-common maven:3.6.0-jdk-11 mvn clean install
+docker run -it --rm -v ~/mt-auth:/usr/src/temp -v "$HOME/.m2":/root/.m2 -v ~/mt-auth/target:/usr/src/temp/target -w /usr/src/temp/mt-common maven:3.6.0-jdk-11 mvn dependency:go-offline -B clean install
 echo 'create mt-access jar'
-docker run -it --rm --name builder -v .m2:/root/.m2 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven/mt-access maven:3.6.0-jdk-11 mvn clean package
+docker run -it --rm -v ~/mt-auth:/usr/src/temp -v "$HOME/.m2":/root/.m2 -v ~/mt-auth/target:/usr/src/temp/target -w /usr/src/temp/mt-access maven:3.6.0-jdk-11 mvn dependency:go-offline -B clean package
 echo 'create mt-access docker image'
 # docker build -f mt-access/Dockerfile . -t mt-auth/mt-access:latest --no-cache
