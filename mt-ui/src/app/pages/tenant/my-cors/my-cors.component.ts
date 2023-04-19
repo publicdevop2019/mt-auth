e581ed0eae339c862105ffdb6d77c89e1cdbc5ec
manytree <publicdevop2019hw@gmail.com>
2023-04-19 22:53:13 +0800

    move cors to tenant

---
+++ mt-ui/src/app/pages/tenant/my-cors/my-cors.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnDestroy } from '@angular/core';
+import { MatBottomSheet, MatBottomSheetConfig } from '@angular/material/bottom-sheet';
+import { FormInfoService } from 'mt-form-builder';
+import { IOption } from 'mt-form-builder/lib/classes/template.interface';
+import { of } from 'rxjs';
+import { IBottomSheet } from 'src/app/clazz/summary.component';
+import { TenantSummaryEntityComponent } from 'src/app/clazz/tenant-summary.component';
+import { ICorsProfile } from 'src/app/clazz/validation/aggregate/cors/interface-cors';
+import { hasValue } from 'src/app/clazz/validation/validator-common';
+import { ISearchConfig } from 'src/app/components/search/search.component';
+import { CORSProfileService } from 'src/app/services/my-cors-profile.service';
+import { DeviceService } from 'src/app/services/device.service';
+import { CorsComponent } from '../cors/cors.component';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from 'src/app/services/project.service';
+import { HttpProxyService } from 'src/app/services/http-proxy.service';
+@Component({
+  selector: 'app-my-cors',
+  templateUrl: './my-cors.component.html',
+  styleUrls: ['./my-cors.component.css']
+})
+export class MyCorsComponent extends TenantSummaryEntityComponent<ICorsProfile, ICorsProfile> implements OnDestroy {
+  public formId = "corsTableColumnConfig";
+  columnList = {
+    id: 'ID',
+    name: 'NAME',
+    description: 'DESCRIPTION',
+    origin: 'CORS_ORIGIN',
+    edit: 'EDIT',
+    clone: 'CLONE',
+    delete: 'DELETE',
+  }
+  sheetComponent = CorsComponent;
+  searchConfigs: ISearchConfig[] = [
+    {
+      searchLabel: 'ID',
+      searchValue: 'id',
+      type: 'text',
+      multiple: {
+        delimiter: '.'
+      }
+    },
+  ]
+  constructor(
+    public entitySvc: CORSProfileService,
+    public deviceSvc: DeviceService,
+    public bottomSheet: MatBottomSheet,
+    public fis: FormInfoService,
+    public route: ActivatedRoute,
+    public projectSvc: ProjectService,
+    public httpSvc: HttpProxyService,
+  ) {
+    super(route, projectSvc, httpSvc, entitySvc, deviceSvc, bottomSheet, fis, 3);
+    const sub2 = this.canDo('VIEW_CORS').subscribe(b => {
+      if (b.result) {
+        this.doSearch({ value: '', resetPage: true })
+      }
+    })
+    this.subs.add(sub2);
+  }
+  getOption(value: string, options: IOption[]) {
+    return options.find(e => e.value == value)
+  }
+  openBottomSheet(id?: string, clone?: boolean): void {
+    let config = new MatBottomSheetConfig();
+    config.autoFocus = true;
+    config.panelClass = 'fix-height'
+    if (hasValue(id)) {
+      of(this.dataSource.data.find(e => e.id === id))
+        .subscribe(next => {
+          if (clone) {
+            config.data = <IBottomSheet<ICorsProfile>>{ context: 'clone', from: next };
+            this.bottomSheet.open(this.sheetComponent, config);
+          } else {
+            config.data = <IBottomSheet<ICorsProfile>>{ context: 'edit', from: next };
+            this.bottomSheet.open(this.sheetComponent, config);
+          }
+        })
+    } else {
+      config.data = <IBottomSheet<ICorsProfile>>{ context: 'new', from: undefined, params: {} };
+      this.bottomSheet.open(this.sheetComponent, config);
+    }
+  }
+  removeFirst(input: string[]) {
+    return input.filter((e, i) => i !== 0);
+  }
+}
---
+++ mt-ui/src/app/services/my-cors-profile.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { IQueryProvider } from 'mt-form-builder/lib/classes/template.interface';
+import { TenantEntityService } from '../clazz/tenant-entity.service';
+import { ICorsProfile } from '../clazz/validation/aggregate/cors/interface-cors';
+import { DeviceService } from './device.service';
+import { HttpProxyService } from './http-proxy.service';
+import { CustomHttpInterceptor } from './interceptors/http.interceptor';
+@Injectable({
+  providedIn: 'root'
+})
+export class CORSProfileService extends TenantEntityService<ICorsProfile, ICorsProfile>  implements IQueryProvider {
+  entityName: string = 'cors';
+  constructor(httpProxy: HttpProxyService, interceptor: CustomHttpInterceptor,deviceSvc:DeviceService) {
+    super(httpProxy, interceptor,deviceSvc);
+  }
+  readByQuery(num: number, size: number, query?: string, by?: string, order?: string, header?: {}){
+     return this.httpProxySvc.readEntityByQuery<ICorsProfile>(this.entityRepo, num, size,query, by, order, header)
+  }
+}
