6c176f245a98b91eaa45a48729a2228878c9c2db
manytree <publicdevop2019hw@gmail.com>
2023-07-12 22:50:25 +0800

    update get start script

--- script/deploy/cluster/mt-access-start.sh
+++ script/deploy/cluster/mt-access-start.sh
@@ -20,11 +20,11 @@ INFRA_IP=
 # eureka registry address, pls add all mt-access instance ip address
 REGISTRY_LIST="http://$INSTANCE_IP:8080/eureka,http://$OTHER_MT_ACCESS_IP:8080/eureka,http://$OTHER_MT_ACCESS_IP:8080/eureka"
 # ---- end of properties-----
-DB_CONFIG="--aws-instance-uri=$DATABASE_IP --spring.datasource.username=$DATABASE_USERNAME --spring.datasource.password=$DATABASE_PWD"
+DB_CONFIG="--spring.datasource.url=jdbc:mysql://$DATABASE_IP:3306/auth_prod?useSSL=false&allowPublicKeyRetrieval=true --spring.datasource.username=$DATABASE_USERNAME --spring.datasource.password=$DATABASE_PWD"
 LOG_CONFIG='--logging.level.com.mt.common.domain.model.domain_event=DEBUG'
 REGISTRY_CONFIG="--eureka.client.serviceUrl.defaultZone=$REGISTRY_LIST --eureka.instance.ip-address=$INSTANCE_IP"
-INFRA_CONFIG="--mt.url.support.dis_lock=redis://$INFRA_IP:6378 --mt.url.support.mq=$INFRA_IP --spring.redis.host=$INFRA_IP --spring.redis.port=6379 --instanceId=$INSTANCE_ID"
-MAIL_CONFIG="--spring.mail.username=$EMAIL_ID --spring.mail.password=$EMAIL_PWD --mt.email.admin=$ADMIN_EMAIL"
+INFRA_CONFIG="--mt.common.url.lock=redis://$INFRA_IP:6378 --mt.common.url.message-queue=$INFRA_IP --spring.redis.host=$INFRA_IP --spring.redis.port=6379 --mt.common.instance-id=$INSTANCE_ID"
+MAIL_CONFIG="--spring.mail.username=$EMAIL_ID --spring.mail.password=$EMAIL_PWD --mt.mgmt.email=$ADMIN_EMAIL"
 PROFILE=--spring.profiles.active=prod
 VM_CONFIG="-Xmx2g -Xms2g -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/logs/$(date +"%Y_%m_%d_%I_%M_%p%z")_heapdump.hprof -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
 SERVER_CONFIG=--server.tomcat.accept-count=100\ --server.tomcat.max-connections=10000\ --server.tomcat.max-threads=10
--- script/deploy/cluster/mt-proxy-start.sh
+++ script/deploy/cluster/mt-proxy-start.sh
@@ -9,8 +9,8 @@ DOMAIN_NAME=
 REGISTRY_LIST="http://$INSTANCE_IP:8080/eureka,http://$OTHER_MT_ACCESS_IP:8080/eureka,http://$OTHER_MT_ACCESS_IP:8080/eureka"
 # ---- end of properties-----
 REGISTRY_CONFIG="--eureka.client.serviceUrl.defaultZone=$REGISTRY_LIST --eureka.instance.ip-address=$INSTANCE_IP"
-INFRA_CONFIG="--mt.url.support.mq=$INFRA_IP --spring.redis.host=$INFRA_IP --spring.redis.port=6379"
-DOMAIN_CONFIG="--manytree.domain-name=$DOMAIN_NAME"
+INFRA_CONFIG="--mt.common.url.lock=redis://$INFRA_IP:6378 --mt.common.url.message-queue=$INFRA_IP --spring.redis.host=$INFRA_IP --spring.redis.port=6379"
+DOMAIN_CONFIG="--mt.common.domain-name=$DOMAIN_NAME"
 DOCKER_CONFIG="-td --rm -v $PWD/./logs/proxy:/logs -p 8111:8111 --log-opt max-size=10m --log-opt max-file=5"
 VM_CONFIG="-Xmx500m -Xms500m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/logs/$(date +"%Y_%m_%d_%I_%M_%p%z")_heapdump.hprof  -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
 IMAGE_CONFIG="--name proxy mt-auth/mt-proxy:latest $VM_CONFIG -jar Proxy.jar"
