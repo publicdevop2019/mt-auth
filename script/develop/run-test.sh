eb7044de851939259feb580df7c7de85a94455cc
manytree <publicdevop2019hw@gmail.com>
2023-08-31 00:33:32 +0800

    update sql script

--- script/develop/run-test.sh
+++ script/develop/run-test.sh
@@ -1,4 +1,8 @@
 cd ../../mt-integration-test
 rm -rf ../logs/analytics
 mkdir ../logs/analytics
-mvn clean test > ../logs/test-run.log
+for VARIABLE in 1 2 3 4 5
+# for VARIABLE in 1
+do
+    mvn clean test > ../logs/test-run.$VARIABLE.log
+done
