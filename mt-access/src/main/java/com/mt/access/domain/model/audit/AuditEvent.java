7ccbd5731502741aceb4f0ee34a25b1b3f4c1bc7
manytree <publicdevop2019hw@gmail.com>
2023-10-03 15:45:11 +0800

    fix audit event empty

--- mt-access/src/main/java/com/mt/access/domain/model/audit/AuditEvent.java
+++ mt-access/src/main/java/com/mt/access/domain/model/audit/AuditEvent.java
@@ -1,7 +1,11 @@
 package com.mt.access.domain.model.audit;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * marker interface, do nothing.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface AuditEvent {
 }
