f5e6e1122821cebedc74c378605e81e0fe9af3df
manytree <publicdevop2019hw@gmail.com>
2023-11-11 11:08:56 +0800

    move market module

--- mt-ui/src/app/services/create-sub-request.service.ts
+++ mt-ui/src/app/services/create-sub-request.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { EntityCommonService } from '../clazz/entity.common-service';
-import { ISubRequest } from '../components/market/subscribe-request/subscribe-request.component';
+import { ISubRequest } from '../pages/tenant/market/subscribe-request/subscribe-request.component';
 import { DeviceService } from './device.service';
 import { HttpProxyService } from './http-proxy.service';
 import { CustomHttpInterceptor } from './interceptors/http.interceptor';
