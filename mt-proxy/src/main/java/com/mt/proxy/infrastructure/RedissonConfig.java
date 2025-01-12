a103cb4faae36f626f91e5991b6120c5893110e8
publicdevop2019 <publicdevop2019hw@gmail.com>
2025-01-12 20:42:42 +0800

    use redisson

---
+++ mt-proxy/src/main/java/com/mt/proxy/infrastructure/RedissonConfig.java
@@ -0,0 +1,36 @@
+package com.mt.proxy.infrastructure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@ConditionalOnProperty(
+    value = "mt.distributed_lock",
+    havingValue = "true",
+    matchIfMissing = true)
+public class RedissonConfig {
+    @Value("${mt.common.url.lock}")
+    private String url;
+    @Value("${mt.common.password.lock:#{null}}")
+    private String password;
+
+    @Bean
+    public RedissonClient configRedisson() {
+        log.debug("start of configure redisson");
+        Config config = new Config();
+        config
+            .useSingleServer()
+            .setAddress(url)
+            .setPassword(password)
+            .setTimeout(1000)
+        ;
+        return Redisson.create(config);
+    }
+}
