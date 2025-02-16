b6aefe00d9da5a438d6fd32e6a2887da3415b796
publicdevop2019 <publicdevop2019hw@gmail.com>
2025-02-16 13:31:57 +0800

    enable metric on mt-access

--- mt-common/src/main/java/com/mt/common/domain/model/develop/MetricsConfig.java
+++ mt-common/src/main/java/com/mt/common/domain/model/develop/MetricsConfig.java
@@ -1,56 +1,16 @@
 package com.mt.common.domain.model.develop;
 
 import com.mt.common.infrastructure.thread_pool.CleanUpThreadPoolExecutor;
-import com.zaxxer.hikari.HikariDataSource;
-import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
-import io.micrometer.core.instrument.binder.system.DiskSpaceMetrics;
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
-import io.micrometer.prometheus.PrometheusConfig;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
-import java.io.File;
-import java.util.concurrent.Executor;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.threads.ThreadPoolExecutor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricsConfig {
-    @Autowired
-    private ServletWebServerApplicationContext serverApplicationContext;
-
-    @Bean
-    public PrometheusMeterRegistry createRegistry() {
-        return new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
-    }
-
-    @Bean
-    public ApplicationRunner monitorJvm(MeterRegistry meterRegistry) {
-        return args -> {
-            new JvmMemoryMetrics().bindTo(meterRegistry);
-            new JvmGcMetrics().bindTo(meterRegistry);
-            new JvmThreadMetrics().bindTo(meterRegistry);
-        };
-    }
-
-    @Bean
-    public ApplicationRunner monitorSystem(MeterRegistry meterRegistry) {
-        return args -> {
-            new DiskSpaceMetrics(new File("/")).bindTo(meterRegistry);
-            new ProcessorMetrics().bindTo(meterRegistry);
-        };
-    }
 
     @Bean
     public FilterRegistrationBean<MetricsFilter> loggingFilter(
@@ -63,13 +23,6 @@ public class MetricsConfig {
         return registrationBean;
     }
 
-    @Bean
-    public ApplicationRunner monitorHikari(HikariDataSource dataSource,
-                                           MeterRegistry meterRegistry) {
-        return args -> {
-            dataSource.setMetricRegistry(meterRegistry);
-        };
-    }
 
     @Bean
     public ApplicationRunner monitorCustomThreadPool(
@@ -89,28 +42,5 @@ public class MetricsConfig {
         };
     }
 
-    @Bean
-    public ApplicationRunner monitorTomcat(MeterRegistry meterRegistry) {
-        return args -> {
-            TomcatWebServer tomcatWebServer =
-                (TomcatWebServer) serverApplicationContext.getWebServer();
-            Connector connector = tomcatWebServer.getTomcat().getConnector();
-            Executor executor = connector.getProtocolHandler().getExecutor();
-            if (executor instanceof ThreadPoolExecutor) {
-                ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
-                Gauge.builder("tomcat.threads.current", threadPoolExecutor,
-                        ThreadPoolExecutor::getPoolSize)
-                    .register(meterRegistry);
-
-                Gauge.builder("tomcat.threads.active", threadPoolExecutor,
-                        ThreadPoolExecutor::getActiveCount)
-                    .register(meterRegistry);
-
-                Gauge.builder("tomcat.threads.max", threadPoolExecutor,
-                        ThreadPoolExecutor::getMaximumPoolSize)
-                    .register(meterRegistry);
-            }
-        };
-    }
 
 }
--- mt-common/src/main/java/com/mt/common/domain/model/develop/MetricsFilter.java
+++ mt-common/src/main/java/com/mt/common/domain/model/develop/MetricsFilter.java
@@ -3,6 +3,9 @@ package com.mt.common.domain.model.develop;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +21,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE + 1)
 public class MetricsFilter extends OncePerRequestFilter {
+    private static final String DOMAIN_PREFIX_REGEX = "^0[A-Z][0-9A-Z]{10}$";
     @Autowired
     private MeterRegistry meterRegistry;
 
@@ -26,10 +30,22 @@ public class MetricsFilter extends OncePerRequestFilter {
                                  FilterChain filterChain) throws IOException, ServletException {
         Timer.Sample sample = Timer.start(meterRegistry);
         filterChain.doFilter(request, response);
-        sample.stop(meterRegistry.timer("http_inbound_request",
+        sample.stop(meterRegistry.timer("http_server_requests",
             "method", request.getMethod(),
             "status", Integer.toString(response.getStatus()),
-            "uri", request.getRequestURI()));
+            "uri", normalize(request.getRequestURI())));
+    }
+
+    private String normalize(String requestURI) {
+        String[] split = requestURI.split("/");
+        List<String> collect = Arrays.stream(split).map(s -> {
+            if (s.matches(DOMAIN_PREFIX_REGEX)) {
+                return "**";
+            } else {
+                return s;
+            }
+        }).collect(Collectors.toList());
+        return String.join("/", collect);
     }
 
 }
